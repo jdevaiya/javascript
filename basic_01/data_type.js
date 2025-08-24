b48439e9a58fc5e74cce6339845bba4e0bc639a2
jdevaiya <jayendradevaiya@gmail.com>
2025-08-24 15:51:45 +0530

    learn datatype

---
+++ basic_01/data_type.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we can use alert, prompt, confirm in browser not  in nodejs
+
+let name = "John"; // string
+let age = 25; // number
+let isApproved = false; // boolean
+let state;
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+// string => '',"",``
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique
+
+
+
+// object => key-value pairs
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object (bug)
