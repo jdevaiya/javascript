22c238ec49cda89a021fd2031f7c80084f811d24
jdevaiya <jayendradevaiya@gmail.com>
2025-08-24 16:16:12 +0530

    comparision

---
+++ basic_01/comparision.js
@@ -0,0 +1,29 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// null and undefined cant use with other values except == and != because they are not converted to numbers
+
+// console.log(null >0);//false
+// console.log(null ==0);//false
+// console.log(null >=0);//true
+
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined >= 0); //false
+// console.log(undefined <= 0); //false
+// console.log(undefined != 0); //true
+// console.log(undefined == undefined); //true
+// console.log(undefined === undefined); //true
+
+
+// === strict equality operator
+// == loose equality operator   
+
+console.log("2"==2); //true
+console.log("2"===2); //false
