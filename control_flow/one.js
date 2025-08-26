a56330d2de2507fe204614783f8610dacc9bda70
jdevaiya <jayendradevaiya@gmail.com>
2025-08-26 19:30:44 +0530

    control flow

---
+++ control_flow/one.js
@@ -0,0 +1,47 @@
+// //  if statement
+// const isUserloggedIn = true
+// const temperature = 50
+
+// if (temperature ===50){
+//     console.log("hot temp");
+// }else{
+//     console.log("cold temp");
+// }
+
+
+// const score = 200 
+// if (score >100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`)
+
+// }
+
+// // console.log(`User power: ${power}`)
+
+
+
+
+const balance = 800
+
+// if (balance>500) console.log("true"),console.log("test"); 
+
+// if(balance < 500){
+//     console.log("less than 500");
+
+// }else if (balance < 750){
+//     console.log("less than 750");
+    
+// }else if(balance < 900){
+//     console.log("less than 1000");
+    
+
+// }else{
+//     console.log("above 1000")
+// }
+
+const UserloggedIn = true
+const debitCard = true
+
+if (UserloggedIn && debitCard ) {
+    console.log("Allow to buy course");
+}
