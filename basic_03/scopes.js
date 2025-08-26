89cb21dcaec5b535f541080a1b3573099ffc1d30
jdevaiya <jayendradevaiya@gmail.com>
2025-08-26 12:52:33 +0530

    scopes & uses

--- basic_03/scopes.js
+++ basic_03/scopes.js
@@ -3,11 +3,56 @@ let a = 300
 if (true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
+
 }
 
 
-console.log(a);
-console.log(b);
 
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Jack"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+// one()
+
+if (true){
+    const username = "John"
+    if (username === "John"){
+        const website = " facebook"
+        // console.log(username + website);
+    }
+    // console.log(website); 
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++ interesting ++++++++++++++++++++++++
+
+
+console.log(addone(5))
+function addone(num){
+    return num +1
+}
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
+
