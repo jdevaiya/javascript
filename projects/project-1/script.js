d88575606ba819b4ad00c0b20a97748484f52ec7
jdevaiya <jayendradevaiya@gmail.com>
2025-08-30 17:11:16 +0530

    project-1

---
+++ projects/project-1/script.js
@@ -0,0 +1,39 @@
+const buttons =document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+
+buttons.forEach(function (button) {
+    // console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id
+        }
+        else if(e.target.id === 'greenyellow'){
+            body.style.backgroundColor = e.target.id
+        }
+        else{
+            body.style.backgroundColor = e.target.id
+        }
+    })
+    
+})
